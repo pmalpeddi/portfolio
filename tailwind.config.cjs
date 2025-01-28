7481b3656389edf219e1c648346205ca56adaee0
HelloMrAntelope <94649939+HelloMrAntelope@users.noreply.github.com>
2025-01-28 16:55:50 -0500

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,11 +19,10 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('../assets/herobg.png')",
+        "hero-pattern": "url('/src/assets/herobg.png')",
       },
     },
   },
   plugins: [],
 };
-
 
