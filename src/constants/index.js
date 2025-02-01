e6032bff76550b5086f1661f52964b0fc7c7d02a
HelloMrAntelope <94649939+HelloMrAntelope@users.noreply.github.com>
2025-02-01 17:14:58 -0500

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -24,7 +24,7 @@ import {
   import FL from "../assets/tech/FL.png"
   import Android from "../assets/tech/Android.png"
   import Ableton from "../assets/tech/Ableton.png"
-  
+  import NA from "../assets/NA.png"
   export const navLinks = [
     {
       id: "about",
@@ -263,7 +263,36 @@ import {
       ],
       image: Puzzle,
       source_code_link: "https://pranav-malpeddi-project2.netlify.app/",
-    }
+    },
+    {
+    name: "To-Do Application",
+    description:
+      "Just started working on a new To-Do application using React Native as of 2/1/2025 to showcase my adaptability and proficiency with JavaScript and React. Upon completion, I plan to deploy the application on the Google Play Store and Microsoft Store.",
+      tags: [
+        {
+          name: "React Native",
+          color: "bg-gradient-to-r from-blue-500 to-cyan-500 bg-clip-text text-transparent",
+        },
+        {
+          name: "JavaScript",
+          color: "bg-gradient-to-r from-yellow-400 to-yellow-600 bg-clip-text text-transparent",
+        },
+        {
+          name: "Tailwind CSS",
+          color: "bg-gradient-to-r from-blue-800 to-blue-300 bg-clip-text text-transparent",
+        },
+        {
+          name: "Google Play Store",
+          color: "bg-gradient-to-r from-green-600 to-green-400 bg-clip-text text-transparent",
+        },
+        {
+          name: "Microsoft Store",
+          color: "bg-gradient-to-r from-gray-700 to-blue-500 bg-clip-text text-transparent",
+        }
+      ],
+      image: NA,
+      source_code_link: "github.com",
+  }
   ];
   
   export { services, technologies, experiences, projects };
